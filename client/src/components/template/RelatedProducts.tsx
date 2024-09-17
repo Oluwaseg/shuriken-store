ba6a5cc3356df584cc4d2c48262d8228b75c5e14
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2024-09-17 02:20:54 +0100

    create component for related products

--- client/src/components/template/RelatedProducts.tsx
+++ client/src/components/template/RelatedProducts.tsx
@@ -1,10 +1,10 @@
-const RelatedProducts = (category: any, subcategory: any) => {
+const RelatedProducts = () => {
   return (
-    <div className="my-24">
-      <div className="text-center text-3xl py-2">{/* title */}</div>
+    <div className='my-24'>
+      <div className='text-center text-3xl py-2'>{/* title */}</div>
 
       {/* products */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-5">
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-5'>
         {/* product layout */}
       </div>
     </div>
