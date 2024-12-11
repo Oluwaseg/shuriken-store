3289df401e6087cffdaac38db56bd7dc88ea28e6
Samuel Oluwatobi Oluwasegun <oluwasegunsam56@gmail.com>
2024-12-11 04:16:07 +0100

    cleaned up console log

--- admin/src/services/apiClient.ts
+++ admin/src/services/apiClient.ts
@@ -8,10 +8,8 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
-    console.log(token);
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
-      console.log(config.headers['Authorization']);
     }
     return config;
   },
